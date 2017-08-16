324c434950b30c9f48b67f5e91f32a20e5231a44
Carrie Ward <carriewardprojects@gmail.com>
2017-08-16 13:19:54 -0700

    C++ source code

---
+++ characterASCII.cpp
@@ -0,0 +1,20 @@
+// characterASCII.cpp : A simple C++ program that displays a character by its ASCII number
+// Carrie Ward 08/16/17
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char letter;
+
+	letter = 65;
+	cout << "The number 65 prints the letter: " << letter << endl;
+
+	letter = 66;
+	cout << "The number 66 prints the letter: " << letter << endl;
+
+	return 0;
+}
+
